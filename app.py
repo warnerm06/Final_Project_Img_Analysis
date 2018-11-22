979b1ef7893251431af1caf2374c6efd601b4515
rooster88 <38765836+warnerm06@users.noreply.github.com>
2018-11-22 10:14:24 -0600

    working flask prediction model

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, url_for
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+
+#Image Analysis dependencies--------------------------------------------
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+from keras.backend import clear_session
+#-----------------------------------------------------------------------
 
 #Database Setup
 #must have "check_same_thread=False" or program will crash
@@ -27,13 +36,48 @@ def index():
     #queries the imageInfo table and returns all results
     results = session.query(imageInfo).all()
     
-    print(type(results))
-    print(type(results[0]))
+    # print(type(results))
+    # print(type(results[0]))
     #print the first row of the query and only the URL column
-    print(results[0].URL)
+    # print(results[0].URL)
     # print(dir(results[0]))
 
     return render_template("index.html")
+
+@app.route("/predict")
+def predict():
+    #Use trained model to predict image
+
+    #Load the image
+    image = cv2.imread("image-classification-keras/image-classification-keras/examples/santa_02.jpg")
+    orig = image.copy()
+
+    #pre-process the image for classification
+    #resize image to fit model shape
+    image = cv2.resize(image, (28, 28))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    #load the trained convolutional neural network
+    model = load_model("image-classification-keras/image-classification-keras/santa_not_santa.model")
+
+    #classify the input image
+    (notSanta, santa) = model.predict(image)[0]
+
+    #build the label
+    label = "Santa" if santa > notSanta else "Not Santa"
+    proba = santa if santa > notSanta else notSanta
+    label = "{}: {:.2f}%".format(label, proba*100)
+
+    #draw the label on the image
+    output = imutils.resize(orig, width=400)
+    cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
+
+    #clear TF session or it will cause an issue upon refreshing page
+    clear_session()
+
+    return render_template("index.html",label=label)
 
 if __name__ == "__main__":
     app.run(debug=True)
